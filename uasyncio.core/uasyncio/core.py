a8883969809688118ef0595f68e76eb1d5aa75f5
Delio Brignoli <dbrignoli@audioscience.com>
2019-12-05 13:32:37 +0100

    Use newer event loop implementation (based on asi2700-master branch)

--- uasyncio.core/uasyncio/core.py
+++ uasyncio.core/uasyncio/core.py
@@ -1,120 +1,148 @@
-try:
-    import utime as time
-except ImportError:
-    import time
-import uheapq as heapq
-import logging
+import utime as time
+import utimeq
+import ucollections
+
+
+type_gen = type((lambda: (yield))())
+
+DEBUG = 0
+log = None
+
+def set_debug(val):
+    global DEBUG, log
+    DEBUG = val
+    if val:
+        import logging
+        log = logging.getLogger("uasyncio.core")
+        log.setLevel(val)
+
 
 class CancelledError(Exception):
     pass
 
-log = logging.getLogger("asyncio")
 
-type_gen = type((lambda: (yield))())
+class TimeoutError(CancelledError):
+    pass
+
 
 class EventLoop:
 
-    def __init__(self):
-        self.q = []
-        self.cnt = 0
+    def __init__(self, runq_len=16, waitq_len=16):
+        self.runq = ucollections.deque((), runq_len, True)
+        self.waitq = utimeq.utimeq(waitq_len)
+        # Current task being run. Task is a top-level coroutine scheduled
+        # in the event loop (sub-coroutines executed transparently by
+        # yield from/await, event loop "doesn't see" them).
+        self.cur_task = None
 
     def time(self):
-        return time.time()
+        return time.ticks_ms()
 
     def create_task(self, coro):
         # CPython 3.4.2
-        self.call_at(0, coro)
+        self.call_later_ms(0, coro)
         # CPython asyncio incompatibility: we don't return Task object
 
     def call_soon(self, callback, *args):
-        self.call_at(self.time(), callback, *args)
+        if __debug__ and DEBUG:
+            log.debug("Scheduling in runq: %s", (callback, args))
+        self.runq.append(callback)
+        if not isinstance(callback, type_gen):
+            self.runq.append(args)
 
     def call_later(self, delay, callback, *args):
-        self.call_at(self.time() + delay, callback, *args)
+        self.call_at_(time.ticks_add(self.time(), int(delay * 1000)), callback, args)
 
-    def call_at(self, time, callback, *args, exc=None):
-        # Including self.cnt is a workaround per heapq docs
-        if __debug__:
-            log.debug("Scheduling %s", (time, self.cnt, callback, args, exc))
-        heapq.heappush(self.q, (time, self.cnt, callback, args, exc, False))
-#        print(self.q)
-        self.cnt += 1
+    def call_later_ms(self, delay, callback, *args):
+        if not delay:
+            return self.call_soon(callback, *args)
+        self.call_at_(time.ticks_add(self.time(), delay), callback, args)
+
+    def call_at_(self, time, callback, args=()):
+        if __debug__ and DEBUG:
+            log.debug("Scheduling in waitq: %s", (time, callback, args))
+        self.waitq.push(time, callback, args)
 
     def wait(self, delay):
         # Default wait implementation, to be overriden in subclasses
         # with IO scheduling
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("Sleeping for: %s", delay)
-        time.sleep(delay)
+        time.sleep_ms(delay)
 
-    def cancel(self, callback, exc = CancelledError):
-        _id = id(callback)
-        for idx, item in enumerate(self.q):
-            t, cnt, cb, args, _exc, _discard = item
-            if id(cb) != _id:
-                continue
-            if __debug__:
-                log.debug("Setting discard flag on: %s at index %d", (t, cnt, cb, args, _exc), idx)
-            self.q[idx] = t, cnt, cb, args, _exc, True
-            self.call_at(0, cb, *args, exc=exc)
-        self.remove_polled_cb(callback)
+    def cancel(self, coro, exc = CancelledError):
+        if isinstance(coro, type_gen):
+            try:
+                prev = coro.pend_throw(exc)
+                if __debug__ and DEBUG:
+                    log.debug("Cancelling %s asynchronously", coro)
+                if prev is False:
+                    _event_loop.remove_polled_cb(coro)
+                    _event_loop.call_soon(coro)
+            except TypeError:
+                # .pend_throw() works only on started coroutines
+                # Kill the coro right here, right now
+                # No need to worry about IO because the coro cannot be registered
+                # because it didn't get a chance to run yet
+                try:
+                    if __debug__ and DEBUG:
+                        log.debug("Cancelling %s synchronously", coro)
+                    coro.throw(exc)
+                except exc:
+                    pass
+        else:
+            raise UnimplementedError('Cancelling a callback is not supported')
 
     def run_forever(self):
+        cur_task = [0, 0, 0]
         while True:
-            if self.q:
-                tnow = self.time()
-                if __debug__:
-                    log.debug('*'*20+' sched step start at %s, num tasks in queue %d', tnow, len(self.q))
-                t, cnt, cb, args, exc, discard = heapq.heappop(self.q)
-                delay = t - tnow
-                if __debug__:
-                    log.debug("Next coroutine to run in %s: %s", delay, (t, cnt, cb, args, exc))
-                if discard:
-                    if __debug__:
-                        log.debug("Discarding: %s", (t, cnt, cb, args, exc, discard))
-                    continue
-#                __main__.mem_info()
-                if delay > 0 and not exc:
-                    self.call_at(t, cb, *args)
-                    self.wait(delay)
-                    continue
-            else:
-                self.wait(-1)
-                # Assuming IO completion scheduled some tasks
-                continue
-            # cancelled callbacks aren't called and nor rescheduled
-            if callable(cb):
-                if not exc:
+            # Expire entries in waitq and move them to runq
+            tnow = self.time()
+            while self.waitq:
+                t = self.waitq.peektime()
+                delay = time.ticks_diff(t, tnow)
+                if delay > 0:
+                    break
+                self.waitq.pop(cur_task)
+                if __debug__ and DEBUG:
+                    log.debug("Moving from waitq to runq: %s", cur_task[1])
+                self.call_soon(cur_task[1], *cur_task[2])
+
+            # Process runq
+            l = len(self.runq)
+            if __debug__ and DEBUG:
+                log.debug("Entries in runq: %d", l)
+            while l:
+                cb = self.runq.popleft()
+                l -= 1
+                args = ()
+                if not isinstance(cb, type_gen):
+                    args = self.runq.popleft()
+                    l -= 1
+                    if __debug__ and DEBUG:
+                        log.info("Next callback to run: %s", (cb, args))
                     cb(*args)
-            else:
+                    continue
+
+                if __debug__ and DEBUG:
+                    log.info("Next coroutine to run: %s", (cb, args))
+                self.cur_task = cb
                 delay = 0
                 try:
-                    if __debug__:
-                        log.debug("Coroutine %s send args: %s, %s", cb, args, exc)
-                    if exc:
-                        try:
-                            ret = cb.throw(exc)
-                        except exc:
-                            # ret == None reschedules a canceled task, next round it should raise StopIteration
-                            ret = None
-                    elif args == ():
+                    if args is ():
                         ret = next(cb)
                     else:
                         ret = cb.send(*args)
-                    if __debug__:
-                        log.debug("Coroutine %s yield result: %s", cb, ret)
+                    if __debug__ and DEBUG:
+                        log.info("Coroutine %s yield result: %s", cb, ret)
                     if isinstance(ret, SysCall1):
                         arg = ret.arg
-                        if isinstance(ret, Sleep):
+                        if isinstance(ret, SleepMs):
                             delay = arg
                         elif isinstance(ret, IORead):
-#                            self.add_reader(ret.obj.fileno(), lambda self, c, f: self.call_soon(c, f), self, cb, ret.obj)
-#                            self.add_reader(ret.obj.fileno(), lambda c, f: self.call_soon(c, f), cb, ret.obj)
-#                            self.add_reader(arg.fileno(), lambda cb: self.call_soon(cb), cb)
                             self.add_reader(arg.fileno(), cb)
                             continue
                         elif isinstance(ret, IOWrite):
-#                            self.add_writer(arg.fileno(), lambda cb: self.call_soon(cb), cb)
                             self.add_writer(arg.fileno(), cb)
                             continue
                         elif isinstance(ret, IOReadDone):
@@ -123,18 +151,56 @@ class EventLoop:
                             self.remove_writer(arg.fileno(), cb)
                         elif isinstance(ret, StopLoop):
                             return arg
+                        else:
+                            assert False, "Unknown syscall yielded: %r (of type %r)" % (ret, type(ret))
                     elif isinstance(ret, type_gen):
                         self.call_soon(ret)
+                    elif isinstance(ret, int):
+                        # Delay
+                        delay = ret
                     elif ret is None:
                         # Just reschedule
                         pass
+                    elif ret is False:
+                        # Don't reschedule
+                        continue
                     else:
                         assert False, "Unsupported coroutine yield value: %r (of type %r)" % (ret, type(ret))
                 except StopIteration as e:
-                    if __debug__:
+                    if __debug__ and DEBUG:
                         log.debug("Coroutine finished: %s", cb)
+                    self.remove_polled_cb(cb)
                     continue
-                self.call_later(delay, cb, *args)
+                except CancelledError as e:
+                    if __debug__ and DEBUG:
+                        log.debug("Coroutine cancelled: %s", cb)
+                    self.remove_polled_cb(cb)
+                    continue
+                except Exception as e:
+                    if log:
+                        log.error("Coroutine %s exception: %s", cb, e)
+                # Currently all syscalls don't return anything, so we don't
+                # need to feed anything to the next invocation of coroutine.
+                # If that changes, need to pass that value below.
+                if delay:
+                    self.call_later_ms(delay, cb)
+                else:
+                    self.call_soon(cb)
+
+            # Wait until next waitq task or I/O availability
+            delay = 0
+            if not self.runq:
+                if self.waitq:
+                    tnow = self.time()
+                    t = self.waitq.peektime()
+                    delay = time.ticks_diff(t, tnow)
+                    if delay < 0:
+                        delay = 0
+                else:
+                    if __debug__ and DEBUG:
+                        log.info("No more tasks to execute, waiting forever")
+                    delay = -1
+            self.wait(delay)
 
     def run_until_complete(self, coro):
         def _run_and_stop():
@@ -142,6 +208,9 @@ class EventLoop:
             yield StopLoop(0)
         self.call_soon(_run_and_stop())
         self.run_forever()
+
+    def stop(self):
+        self.call_soon((lambda: (yield StopLoop(0)))())
 
     def close(self):
         pass
@@ -161,9 +230,6 @@ class SysCall1(SysCall):
     def __init__(self, arg):
         self.arg = arg
 
-class Sleep(SysCall1):
-    pass
-
 class StopLoop(SysCall1):
     pass
 
@@ -182,14 +248,77 @@ class IOWriteDone(SysCall1):
 
 _event_loop = None
 _event_loop_class = EventLoop
-def get_event_loop():
+def get_event_loop(runq_len=16, waitq_len=16):
     global _event_loop
     if _event_loop is None:
-        _event_loop = _event_loop_class()
+        _event_loop = _event_loop_class(runq_len, waitq_len)
     return _event_loop
 
 def sleep(secs):
-    yield Sleep(secs)
+    yield int(secs * 1000)
+
+# Implementation of sleep_ms awaitable with zero heap memory usage
+class SleepMs(SysCall1):
+
+    def __init__(self):
+        self.v = None
+        self.arg = None
+
+    def __call__(self, arg):
+        self.v = arg
+        #print("__call__")
+        return self
+
+    def __iter__(self):
+        #print("__iter__")
+        return self
+
+    def __next__(self):
+        if self.v is not None:
+            #print("__next__ syscall enter")
+            self.arg = self.v
+            self.v = None
+            return self
+        #print("__next__ syscall exit")
+        _stop_iter.__traceback__ = None
+        raise _stop_iter
+
+_stop_iter = StopIteration()
+sleep_ms = SleepMs()
+
+
+def cancel(coro, exc=CancelledError):
+    _event_loop.cancel(coro, exc=exc)
+
+
+class TimeoutObj:
+    def __init__(self, coro):
+        self.coro = coro
+
+
+def wait_for_ms(coro, timeout):
+
+    def waiter(coro, timeout_obj):
+        res = yield from coro
+        if __debug__ and DEBUG:
+            log.debug("waiter: cancelling %s", timeout_obj)
+        timeout_obj.coro = None
+        return res
+
+    def timeout_func(timeout_obj):
+        if timeout_obj.coro:
+            if __debug__ and DEBUG:
+                log.debug("timeout_func: cancelling %s", timeout_obj.coro)
+            cancel(timeout_obj.coro, exc=TimeoutError())
+
+    timeout_obj = TimeoutObj(_event_loop.cur_task)
+    _event_loop.call_later_ms(timeout, timeout_func, timeout_obj)
+    return (yield from waiter(coro, timeout_obj))
+
+
+def wait_for(coro, timeout):
+    return wait_for_ms(coro, int(timeout * 1000))
+
 
 def coroutine(f):
     return f
--- uasyncio/uasyncio/__init__.py
+++ uasyncio/uasyncio/__init__.py
@@ -1,13 +1,25 @@
-import errno
+import uerrno as errno
 import uselect as select
 import usocket as _socket
 from uasyncio.core import *
 
 
+DEBUG = 0
+log = None
+
+def set_debug(val):
+    global DEBUG, log
+    DEBUG = val
+    if val:
+        import logging
+        log = logging.getLogger("uasyncio")
+        log.setLevel(val)
+
+
 class EpollEventLoop(EventLoop):
 
-    def __init__(self):
-        EventLoop.__init__(self)
+    def __init__(self, runq_len=16, waitq_len=16):
+        EventLoop.__init__(self, runq_len, waitq_len)
         self.poller = select.poll()
         self.objmap = {}
 
@@ -16,7 +28,10 @@ class EpollEventLoop(EventLoop):
         try:
             self.poller.unregister(fd)
         except OSError as e:
-            if e.args[0] != errno.ENOENT:
+            if e.args[0] == errno.ENOENT:
+                if __debug__ and DEBUG:
+                    log.debug("_unregister_fd() attepted to remove unregistered FD %s", fd)
+            else:
                 raise
 
     def remove_polled_cb(self, cb):
@@ -27,7 +42,7 @@ class EpollEventLoop(EventLoop):
                 self._unregister_fd(fd)
 
     def add_reader(self, fd, cb, *args):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("add_reader%s", (fd, cb, args))
         cbs = self.objmap.setdefault(fd, {})
         self.poller.register(fd, select.POLLIN)
@@ -37,7 +52,7 @@ class EpollEventLoop(EventLoop):
             cbs[id(cb)] = (cb, None)
 
     def remove_reader(self, fd, cb):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("remove_reader(%s)", (fd, cb))
         cbs = self.objmap.get(fd, {})
         cbs.pop(id(cb), None)
@@ -45,7 +60,7 @@ class EpollEventLoop(EventLoop):
             self._unregister_fd(fd)
 
     def add_writer(self, fd, cb, *args):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("add_writer%s", (fd, cb, args))
         cbs = self.objmap.setdefault(fd, {})
         self.poller.register(fd, select.POLLOUT)
@@ -55,7 +70,7 @@ class EpollEventLoop(EventLoop):
             cbs[id(cb)] = (cb, None)
 
     def remove_writer(self, fd, cb):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("remove_writer(%s)", fd)
         cbs = self.objmap.get(fd, {})
         cbs.pop(id(cb), None)
@@ -63,17 +78,14 @@ class EpollEventLoop(EventLoop):
             self._unregister_fd(fd)
 
     def wait(self, delay):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("epoll.wait(%s)", delay)
             for fd, cbs in self.objmap.items():
                 for cb, args in cbs.values():
                     log.debug("epoll.registered(%d) %s", fd, (cb, args))
 
         # We need one-shot behavior (second arg of 1 to .poll())
-        if delay == -1:
-            res = self.poller.poll(-1, 1)
-        else:
-            res = self.poller.poll(int(delay * 1000), 1)
+        res = self.poller.poll(delay, 1)
         #log.debug("epoll result: %s", res)
         for fd, ev in res:
             # Remove the registered callbacks dictionary from its parent
@@ -83,18 +95,18 @@ class EpollEventLoop(EventLoop):
             if not cbs:
                 log.error("Event %d on fd %r but no callback registered", ev, fd)
                 continue
-            if __debug__:
+            if __debug__ and DEBUG:
                 s = '\n'.join(str(v) for v in cbs.values())
                 log.debug("Matching IO callbacks for %r:\n%s", (fd, ev), s)
             while cbs:
                 _id, data = cbs.popitem()
                 cb, args = data
                 if args is None:
-                    if __debug__:
+                    if __debug__ and DEBUG:
                         log.debug("Scheduling IO coro: %r", (fd, ev, cb))
                     self.call_soon(cb)
                 else:
-                    if __debug__:
+                    if __debug__ and DEBUG:
                         log.debug("Calling IO callback: %r", (fd, ev, cb, args))
                     cb(*args)
             # If no callback registered an event for this fd unregister it
@@ -119,10 +131,10 @@ class StreamReader:
         return res
 
     def readline(self):
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("StreamReader.readline()")
         yield IORead(self.s)
-#        if __debug__:
+#        if __debug__ and DEBUG:
 #            log.debug("StreamReader.readline(): after IORead: %s", s)
         while True:
             res = self.s.readline()
@@ -131,7 +143,7 @@ class StreamReader:
             log.warning("Empty read")
         if not res:
             yield IOReadDone(self.s)
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("StreamReader.readline(): res: %s", res)
         return res
 
@@ -156,25 +168,25 @@ class StreamWriter:
         # to return immediately (which means it has to buffer all the
         # data), this method is a coroutine.
         sz = len(buf)
-        if __debug__:
+        if __debug__ and DEBUG:
             log.debug("StreamWriter.awrite(): spooling %d bytes", sz)
         while True:
             res = self.s.write(buf)
             # If we spooled everything, return immediately
             if res == sz:
-                if __debug__:
+                if __debug__ and DEBUG:
                     log.debug("StreamWriter.awrite(): completed spooling %d bytes", res)
                 return
             if res is None:
                 res = 0
-            if __debug__:
+            if __debug__ and DEBUG:
                 log.debug("StreamWriter.awrite(): spooled partial %d bytes", res)
             assert res < sz
             buf = buf[res:]
             sz -= res
             yield IOWrite(self.s)
             #assert s2.fileno() == self.s.fileno()
-            if __debug__:
+            if __debug__ and DEBUG:
                 log.debug("StreamWriter.awrite(): can write more")
 
     def aclose(self):
@@ -189,7 +201,7 @@ class StreamWriter:
 
 
 def open_connection(host, port):
-    if __debug__:
+    if __debug__ and DEBUG:
         log.debug("open_connection(%s, %s)", host, port)
     s = _socket.socket()
     s.setblocking(False)
@@ -200,39 +212,42 @@ def open_connection(host, port):
     except OSError as e:
         if e.args[0] != errno.EINPROGRESS:
             raise
-    if __debug__:
+    if __debug__ and DEBUG:
         log.debug("open_connection: After connect")
     yield IOWrite(s)
-#    if __debug__:
+#    if __debug__ and DEBUG:
 #        assert s2.fileno() == s.fileno()
-    if __debug__:
+    if __debug__ and DEBUG:
         log.debug("open_connection: After iowait: %s", s)
     return StreamReader(s), StreamWriter(s, {})
 
 
 def start_server(client_coro, host, port, backlog=10):
-    log.debug("start_server(%s, %s)", host, port)
-    s = _socket.socket()
-    s.setblocking(False)
+    if DEBUG and __debug__:
+        log.debug("start_server(%s, %s)", host, port)
+    ai = _socket.getaddrinfo(host, port, 0, _socket.SOCK_STREAM)
+    ai = ai[0]
+    s = _socket.socket(ai[0], ai[1], ai[2])
+    try:
+        s.setblocking(False)
 
-    ai = _socket.getaddrinfo(host, port)
-    addr = ai[0][4]
-    s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
-    s.bind(addr)
-    s.listen(backlog)
-    while True:
-        if __debug__:
-            log.debug("start_server: Before accept")
-        yield IORead(s)
-        if __debug__:
-            log.debug("start_server: After iowait")
-        s2, client_addr = s.accept()
-        s2.setblocking(False)
-        if __debug__:
-            log.debug("start_server: After accept: %s", s2)
-        extra = {"peername": client_addr}
-        yield client_coro(StreamReader(s2), StreamWriter(s2, extra))
-
+        s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
+        s.bind(ai[-1])
+        s.listen(backlog)
+        while True:
+            if DEBUG and __debug__:
+                log.debug("start_server: Before accept")
+            yield IORead(s)
+            if DEBUG and __debug__:
+                log.debug("start_server: After iowait")
+            s2, client_addr = s.accept()
+            s2.setblocking(False)
+            if DEBUG and __debug__:
+                log.debug("start_server: After accept: %s", s2)
+            extra = {"peername": client_addr}
+            yield client_coro(StreamReader(s2), StreamWriter(s2, extra))
+    finally:
+        s.close()
 
 import uasyncio.core
 uasyncio.core._event_loop_class = EpollEventLoop
