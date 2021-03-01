36a700ab34b07fa91b049421d79a11bdddb1832c
Delio Brignoli <dbrignoli@audioscience.com>
2021-03-01 12:16:32 +0000

    multiprocessing: open Connection() fd in binary mode

--- multiprocessing/multiprocessing.py
+++ multiprocessing/multiprocessing.py
@@ -39,7 +39,7 @@ class Connection:
 
     def __init__(self, fd):
         self.fd = fd
-        self.f = open(fd)
+        self.f = open(fd, 'b')
 
     def __repr__(self):
         return "<Connection %s>" % self.f
