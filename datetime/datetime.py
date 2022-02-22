656239efb2ab1c15b02ade639db2032e131b981d
Delio Brignoli <dbrignoli@audioscience.com>
2022-02-22 13:49:55 +0000

    datetime: add  __new__() to tzinfo class

--- datetime/datetime.py
+++ datetime/datetime.py
@@ -914,6 +914,11 @@ class tzinfo:
     Subclasses must override the name(), utcoffset() and dst() methods.
     """
     __slots__ = ()
+
+    def __new__(self, *args, **kwargs):
+         """Constructor."""
+         return object.__new__(self)
+
     def tzname(self, dt):
         "datetime -> string name of time zone."
         raise NotImplementedError("tzinfo subclass must override tzname()")
