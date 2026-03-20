3aa4a4418dba801317b077b945103c711f5416a1
Delio Brignoli <dbrignoli@audioscience.com>
2026-03-20 22:07:49 +0000

    unix-ffi/socket: Use C socket type directly instead of Python subclass.
    MicroPython's stream protocol inheritance is broken for Python
    subclasses of C types: the C-level MP_STREAM_GET_FILENO ioctl reads
    from the wrong object layout, returning a garbage fd.  This causes
    select.poll / asyncio to poll the wrong descriptor.  Remove the
    socket subclass and use _socket.socket directly, moving accept's
    address translation to a standalone helper.

--- unix-ffi/socket/socket.py
+++ unix-ffi/socket/socket.py
@@ -27,33 +27,28 @@ def inet_aton(addr):
     return inet_pton(AF_INET, addr)
 
 
+# Do NOT subclass _socket.socket in Python: MicroPython's stream protocol
+# inheritance is broken for Python subclasses of C types -- the C-level
+# MP_STREAM_GET_FILENO ioctl reads from the mp_obj_instance_t layout
+# (members map) instead of the mp_obj_socket_t layout, returning a garbage
+# fd value.  This causes select.poll / asyncio to poll the wrong fd.
+# Use the C type directly and provide helpers as standalone functions.
+socket = _socket.socket
+
+
+def _wrap_accept(sock):
+    """Accept a connection, returning (new_socket, (ip_str, port))."""
+    s, addr = sock.accept()
+    addr = _socket.sockaddr(addr)
+    return (s, (_socket.inet_ntop(addr[0], addr[1]), addr[2]))
+
+
 def create_connection(addr, timeout=None, source_address=None):
     s = socket()
-    # print("Address:", addr)
     ais = getaddrinfo(addr[0], addr[1])
-    # print("Address infos:", ais)
     for ai in ais:
         try:
             s.connect(ai[4])
             return s
         except:
             pass
-
-
-class socket(_socket.socket):
-    def accept(self):
-        s, addr = super().accept()
-        addr = _socket.sockaddr(addr)
-        return (s, (_socket.inet_ntop(addr[0], addr[1]), addr[2]))
-
-    def bind(self, addr):
-        return super().bind(_resolve_addr(addr))
-
-    def connect(self, addr):
-        return super().connect(_resolve_addr(addr))
-
-    def sendall(self, *args):
-        return self.send(*args)
-
-    def sendto(self, data, addr):
-        return super().sendto(data, _resolve_addr(addr))
