8a184c4b808cc252bf428298ce71001ab3af03bb
Delio Brignoli <dbrignoli@audioscience.com>
2026-03-20 22:22:33 +0000

    unix-ffi/os.linux: Add Linux-specific system call wrappers.
    New package providing ioctl helpers, mount/umount/reboot, execv/execvp,
    setenv, realpath, and device access modules for MTD, SPI, I2C, evdev,
    block devices, syslog and monotonic clocks via FFI.

---
+++ unix-ffi/os.linux/manifest.py
@@ -0,0 +1,6 @@
+metadata(version="0.1.0", description="Linux-specific system call wrappers (ioctl, mount, reboot, MTD, SPI, I2C, evdev, syslog, clock_gettime).")
+
+require("ffilib")
+require("os")
+
+package("os/linux")
---
+++ unix-ffi/os.linux/os/linux/__init__.py
@@ -0,0 +1,71 @@
+
+import ffilib
+import os
+
+
+libc = ffilib.libc()
+sleep = libc.func('I', 'sleep', 'I')
+_mount = libc.func('i', 'mount', 'sssLs')
+_umount = libc.func('i', 'umount', 's')
+_setenv = libc.func('i', 'setenv', 'ssi')
+_reboot_syscall = libc.func('l', 'syscall', 'liiis')
+_realpath = libc.func("s", "realpath", "ss")
+_free = libc.func("v", "free", "p")
+
+
+
+LINUX_REBOOT_MAGIC1         = 0xfee1dead
+LINUX_REBOOT_MAGIC2         = 672274793
+LINUX_REBOOT_CMD_RESTART	= 0x01234567
+LINUX_REBOOT_CMD_HALT		= 0xCDEF0123
+LINUX_REBOOT_CMD_CAD_ON		= 0x89ABCDEF
+LINUX_REBOOT_CMD_CAD_OFF	= 0x00000000
+LINUX_REBOOT_CMD_POWER_OFF	= 0x4321FEDC
+LINUX_REBOOT_CMD_RESTART2	= 0xA1B2C3D4
+LINUX_REBOOT_CMD_SW_SUSPEND	= 0xD000FCE2
+LINUX_REBOOT_CMD_KEXEC		= 0x45584543
+
+
+def reboot(cmd, arg_str):
+	SYS_reboot = 142
+	e = _reboot_syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, cmd, arg_str)
+	os.check_error(e)
+
+
+def mount(source, target, fstype, flags = 0, opts = None):
+	e = _mount(source, target, fstype, flags, opts)
+	os.check_error(e)
+
+
+def umount(target):
+	e = _umount(target)
+	os.check_error(e)
+
+
+def execv(path, args = []):
+	assert args, '`args` argument cannot be empty'
+	_args = [path] + args + [None]
+	_execl = libc.func('i', 'execl', 's'*len(_args))
+	e = _execl(*_args)
+	os.check_error(e)
+
+
+def execvp(executable, args = []):
+	assert args, '`args` argument cannot be empty'
+	_args = [executable] + args + [None]
+	_execlp = libc.func('i', 'execlp', 's'*len(_args))
+	e = _execlp(*_args)
+	os.check_error(e)
+
+
+def setenv(name, value, overwrite = True):
+	e = _setenv(name, value, 1 if overwrite else 0)
+	os.check_error(e)
+
+def realpath(path):
+	p = _realpath(path, None)
+	# Do not free the string here, it is tracked by upy and freed by it.
+	# _free(p)
+	if p == None:
+		os.raise_error()
+	return p
